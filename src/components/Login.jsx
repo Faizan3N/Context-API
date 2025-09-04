2eae6fe126bc390f762f159d63cc4f8c6b0ca5f2
Faizan <faizanali3n@gmail.com>
2025-09-04 16:39:38 +0500

    Login.jsx added

---
+++ src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React, {useState, useContext} from 'react'
+import UserContext from '../context/Usercontext'
+
+function Login() {
+
+
+    const[username, setUsername] = useState('')
+    const[password, setPassword] = useState('')
+
+    const [user, setUser] = useContext(UserContext)
+
+    const handleSubmit = () => {
+        console.log(username, password) 
+}
+  return (
+    <div>
+
+        <h2>Login</h2>
+    <input
+      type="text"
+      value={username}
+      onChange={e => setUsername(e.target.value)}
+      placeholder='Username'
+    />
+    <input
+      type="password"
+      value={password}
+      onChange={e => setPassword(e.target.value)}
+      placeholder='Password'
+    />
+    <button onClick={handleSubmit}>Submit</button>
+    </div>
+  )
+}
+
+export default Login
